2d1e3c541f9b5ad0e045b4ca64de7d51256fd028
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-12-01 11:54:40 +0100

    Datababase is ready to use with product entity now

--- app/src/main/java/com/example/test_design/data/dao/ProductDao.kt
+++ app/src/main/java/com/example/test_design/data/dao/ProductDao.kt
@@ -1,2 +1,15 @@
-package com.example.test_design.data.dao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
+@Dao
+interface ProductDao {
+    @Insert
+    suspend fun insert(product: Product)
+
+    @Query("SELECT * FROM products")
+    suspend fun getAll(): List<Product>
+
+    @Query("DELETE FROM products")
+    suspend fun deleteAll()
+}
